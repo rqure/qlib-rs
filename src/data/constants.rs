d343b05e1b8a4be24dc7a6e51f5f8bd668e6fa4c
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2025-06-26 16:49:11 +0000

    refactor: Remove unused entity-related constants for code simplification

--- src/data/constants.rs
+++ src/data/constants.rs
@@ -1,13 +1,2 @@
 
 pub const INDIRECTION_DELIMITER: &str = "->";
-
-pub mod et {
-    pub const OBJECT: &str = "Object"; // All entity types inherit from this
-    pub const ROOT : &str = "Root";
-}
-
-pub mod ft {
-    pub const NAME : &str = "Name"; // Belongs to Object
-    pub const PARENT: &str = "Parent"; // Belongs to Object
-    pub const CHILDREN: &str = "Children"; // Belongs to Object
-}
