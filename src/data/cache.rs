34314780a01fef690012a8e8843cff1d6655b089
Rizwan Qureshi <rizzlethefizzle@gmail.com>
2025-09-20 16:49:32 +0000

    refactor: Simplify process_notification method by removing notification parameter and restructuring logic

--- src/data/cache.rs
+++ src/data/cache.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use rustc_hash::FxHashMap;
 
 use crate::{
-    data::StoreTrait, EntityId, EntityType, FieldType, Notification, NotificationQueue, NotifyConfig, Request, Value
+    data::StoreTrait, EntityId, EntityType, FieldType, NotificationQueue, NotifyConfig, Request, Value
 };
 
 #[derive(Debug)]
@@ -132,34 +132,36 @@ impl Cache {
 }
 
 impl Cache {
-    pub fn process_notification(&mut self, notification: Notification) {
+    pub fn process_notification(&mut self) {
         // Extract entity_id and field_type from the current request
-        if let Request::Read { entity_id, field_types, value: current_value, .. } = &notification.current {
-            if let Request::Read { value: previous_value, .. } = &notification.previous {
-                // Handle the case where field_types is Vec<FieldType> - take the first one
-                if let Some(field_type) = field_types.first() {
-                    if let Some(curr_val) = current_value {
-                        self.fields_by_entity_id
-                            .entry(*entity_id)
-                            .or_default()
-                            .insert(*field_type, curr_val.clone());
-                    }
-
-                    // If the field type is one of the index fields, we need to update the index
-                    if self.index_fields.contains(field_type) {
-                        // Remove old entry if it exists
-                        if let Some(prev_val) = previous_value {
-                            let old_index_key = self.make_index_key(*entity_id, *field_type, prev_val);
-                            self.entity_ids_by_index_fields.remove(&old_index_key);
-                        }
-                        
-                        // Add new entry
+        while let Some(notification) = self.notify_queue.pop() {
+            if let Request::Read { entity_id, field_types, value: current_value, .. } = &notification.current {
+                if let Request::Read { value: previous_value, .. } = &notification.previous {
+                    // Handle the case where field_types is Vec<FieldType> - take the first one
+                    if let Some(field_type) = field_types.first() {
                         if let Some(curr_val) = current_value {
-                            let new_index_key = self.make_index_key(*entity_id, *field_type, curr_val);
-                            self.entity_ids_by_index_fields
-                                .entry(new_index_key)
-                                .or_insert_with(Vec::new)
-                                .push(*entity_id);
+                            self.fields_by_entity_id
+                                .entry(*entity_id)
+                                .or_default()
+                                .insert(*field_type, curr_val.clone());
+                        }
+
+                        // If the field type is one of the index fields, we need to update the index
+                        if self.index_fields.contains(field_type) {
+                            // Remove old entry if it exists
+                            if let Some(prev_val) = previous_value {
+                                let old_index_key = self.make_index_key(*entity_id, *field_type, prev_val);
+                                self.entity_ids_by_index_fields.remove(&old_index_key);
+                            }
+                            
+                            // Add new entry
+                            if let Some(curr_val) = current_value {
+                                let new_index_key = self.make_index_key(*entity_id, *field_type, curr_val);
+                                self.entity_ids_by_index_fields
+                                    .entry(new_index_key)
+                                    .or_insert_with(Vec::new)
+                                    .push(*entity_id);
+                            }
                         }
                     }
                 }
